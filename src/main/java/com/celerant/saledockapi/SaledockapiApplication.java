77139ab9a91452c07e2e8a924f672b100e3683f1
mlpocprojects <mlpoc.projects@gmail.com>
2025-09-03 13:00:09 -0400

    Initial Project - Skeleton

---
+++ src/main/java/com/celerant/saledockapi/SaledockapiApplication.java
@@ -0,0 +1,13 @@
+package com.celerant.saledockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaledockapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaledockapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/celerant/saledockapi/SaledockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celerant.saledockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaledockapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
